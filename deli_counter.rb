fee9f3f4b59804b2262f495665e1b6e755732716
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-24 12:06:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, customer_name)
-  katz_deli << customer_name
+  katz_deli.push(customer_name)
   puts "Welcome, #{customer_name}. You are number #{katz_deli.length} in line."
 end
 
